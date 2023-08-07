c2f90954eb27b8b7ce561ea54815edf763100566
Cen Hu <tsen.hu@gmail.com>
2023-08-07 11:34:12 -0500

    finished pages tts and mp3 merge, still need to think about gtts rate limits and better voice

--- pdf_voice.py
+++ pdf_voice.py
@@ -11,13 +11,22 @@ import time as t
 from pydub import AudioSegment
 import os
 import io
-
+import shutil
 
 path = 'c:/Users/tsenh/GitHub/'
 
-input_pdf_path = path + 'input_test/renter insurance.pdf'
-output_mp3_path = path + "output_test/renter insurance.mp3"
+input_pdf_path = path + 'input_test/50YearsDataScience.pdf'
+output_mp3_path = path + "output_test/50YearsDataScience.mp3"
 temp_folder = path + 'input_test/temp/'    
+
+#clean the temp_folder first
+mp3_list = os.listdir(temp_folder)
+
+for m in mp3_list:
+    
+    os.unlink(temp_folder+m)
+    
+    
 def text_to_speech(text, language='en', slow=False):
     tts = gTTS(text=text, lang=language, slow=slow)
     return tts   
@@ -37,11 +46,18 @@ with open(input_pdf_path, 'rb') as file:
         audio_segments.append(AudioSegment.from_file(os.path.devnull, format="mp3", frame_rate=44100, channels=2, sample_width=2, data=audio_segment))
         '''
         audio_data = text_to_speech(text, language = 'en', slow=False)
-        audio_segment = AudioSegment.from_file(io.BytesIO(audio_data), format="mp3")
-        audio_segments.append(audio_segment)
-        
-combined_audio = audio_segments[0]
-for audio_segment in audio_segments[1:]:
-    combined_audio += audio_segment
-        
-audio_segment.save(combined_audio, output_mp3_path)
+        audio_data.save(temp_folder+str(page_num)+'.mp3')
+        print('Page {} has been processed.'.format(page_num))
+        t.sleep(10)
+
+
+
+mp3_list = os.listdir(temp_folder)
+
+combined_audio = AudioSegment.from_mp3(temp_folder+mp3_list[0])
+
+for mp3 in mp3_list[1:]:
+    combined_audio += AudioSegment.from_mp3(temp_folder+mp3)
+
+
+combined_audio.export(output_mp3_path, format='mp3')        
